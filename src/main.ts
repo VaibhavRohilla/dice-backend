094ee5714bbceef3f6adec9c7e8a136006ee5abe
Vaibhav <vaibhavrohilla2000@gmail.com>
2025-12-17 23:05:35 +0530

    Updated EnvExample

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+await app.listen(port);
+console.log(`Server is running on port ${port}`);
+
   console.log(`Server is running on port ${process.env.PORT}`);
 }
 
